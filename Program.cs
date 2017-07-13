e6db42438aa1a15a6b4607b7b9dcdfa8f90722fd
Mitchell Blom <mitchellblom@gmail.com>
2017-07-13 13:56:07 -0500

    populated classes according to team interface

--- Program.cs
+++ Program.cs
@@ -24,60 +24,98 @@ namespace dreamteam
             Andy andy = new Andy();
             Jordan jordan = new Jordan();
 
-
-
         }
     }
 
     public class Ben : IDreamTeam
     {
-        public string Specialty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Specialty { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
         public void Work()
         {
-            Console.WriteLine("Ben works by casting spells on his machine. Known for producing thousands of lines without ever touching his keyboard.")
+            Console.WriteLine("Ben works by casting D&D spells on his machine. Known for producing thousands of lines without ever touching his keyboard.");
         }
     }
 
     public class Madeline : IDreamTeam
     {
-        public string Specialty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
         public void Work()
         {
-            Console.WriteLine("Finds funniest videos to lighten the mood and keep the team positive.")
+            Console.WriteLine("Finds funny YouTube videos to lighten the mood and keep the team positive.");
         }
     }
 
-    public class Mitchell
+    public class Mitchell : IDreamTeam
     {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
+        public void Work()
+        {
+            Console.WriteLine("Harmonizes conflict and also harmonizes when someone begins singing.");
+        }
     }
 
     public class Chaz
     {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
+        public void Work()
+        {
+            Console.WriteLine("Doesn't like the available libraries; writes his own.");
+        }
     }
 
     public class Matt
     {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
+        public void Work()
+        {
+            Console.WriteLine("Masters concepts effortlessly by tatooing them into memory and onto sleeve.");
+        }
     }
 
     public class Andy
     {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
+        public void Work()
+        {
+            Console.WriteLine("Networking guru. Once got a contract from approaching the right person. It's doable!");
+        }
     }
 
     public class Jordan
     {
+        public string Specialty { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
 
+        public void Work()
+        {
+            Console.WriteLine("Cooks healthy and delicious meals for the team. Knows all the bus routes of Davidson County.");
+        }
     }
 
 
