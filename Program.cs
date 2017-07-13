18842facd9f5c2e889e91f15cccea3bfb1d928fb
Mitchell Blom <mitchellblom@gmail.com>
2017-07-13 11:34:15 -0500

    classes made but not all populated

--- Program.cs
+++ Program.cs
@@ -2,11 +2,83 @@
 
 namespace dreamteam
 {
+
+    public interface IDreamTeam
+    {
+        string Specialty { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string FullName { get; set; }
+        void Work();
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Ben ben = new Ben();
+            Madeline madeline = new Madeline();
+            Chaz chaz = new Chaz();
+            Matt matt = new Matt();
+            Andy andy = new Andy();
+            Jordan jordan = new Jordan();
+
+
+
         }
     }
+
+    public class Ben : IDreamTeam
+    {
+        public string Specialty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Work()
+        {
+            Console.WriteLine("Ben works by casting spells on his machine. Known for producing thousands of lines without ever touching his keyboard.")
+        }
+    }
+
+    public class Madeline : IDreamTeam
+    {
+        public string Specialty { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FullName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Work()
+        {
+            Console.WriteLine("Finds funniest videos to lighten the mood and keep the team positive.")
+        }
+    }
+
+    public class Mitchell
+    {
+
+    }
+
+    public class Chaz
+    {
+
+    }
+
+    public class Matt
+    {
+
+    }
+
+    public class Andy
+    {
+
+    }
+
+    public class Jordan
+    {
+
+    }
+
+
 }
